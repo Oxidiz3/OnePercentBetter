1280fe65b5576021f7d3e66f42a44fbb16d5f7f8
Shane <mecham.shane@gmail.com>
2021-02-05 14:01:14 -0700

    Created a Database class that will handle the sqlite transactions.

--- database.py
+++ database.py
@@ -2,57 +2,44 @@ import sqlite3
 from sqlite3 import Error
 
 
-def create_connection(db_file):
-    """ Connects the database to the program and returns a Connection object.
-        :param db_file: Path to the database file.
-        :return: Connection Object or none.
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    return conn
+class Database:
+    def create_table(self, create_table_sql) -> bool:
+        """ creates a table given a Connection object and a sql statement.
+        :param create_table_sql: a CREATE TABLE statement
+        :return:
+        """
+        try:
+            c = self.conn.cursor()
+            c.execute(create_table_sql)
+            return True
+        except Error as e:
+            print(e)
+            return False
 
+    def __init__(self, db_file=r'Database/onepercent.db'):
+        """ Initializes the database for the app, it will handle all of the setup required for getting the database running
+            and started.
+            :param db_file: a string to the database path.
+        """
 
-def create_table(conn, create_table_sql):
-    """ creates a table given a Connection object and a sql statement.
-    :param conn: Connection Object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-        return True
-    except Error as e:
-        print(e)
-        return False
+        self.conn = None
+        try:
+            self.conn = sqlite3.connect(db_file)
+            print(sqlite3.version)
+        except Error as e:
+            print(e)
 
-
-def init(databasePath):
-    """ Initializes the database for the app, it will handle all of the setup required for getting the database running
-        and started.
-
-        :param databasePath: a string to the database path.
-    """
-    sql_create_goals_table = """ CREATE TABLE IF NOT EXISTS goals (
-                                    id integer PRIMARY KEY,
-                                    start integer NOT NULL,
-                                    end integer NOT NULL
-                                    );
-                             """
-    conn = create_connection(databasePath)
-
-    if conn is not None:
-        create_table(conn, sql_create_goals_table)
-    else:
-        print("Error! cannot create the database connection.")
-    if conn:
-        conn.close()
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
 
 
 if __name__ == '__main__':
-    init(r"Database/onepercent.db")
-
+    carl = Database()
+    sql_create_goals_table = """ CREATE TABLE IF NOT EXISTS goals (
+                                            id integer PRIMARY KEY,
+                                            start integer NOT NULL,
+                                            end integer NOT NULL
+                                            );
+                                     """
+    carl.close_connection()
