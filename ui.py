38f0f72483da2ee34d7a89fd1bcef3c4b1a89ba2
Oxidiz3 <mecham.porter@gmail.com>
2021-02-06 15:49:30 -0700

    Working on the wording of my goals

--- ui.py
+++ ui.py
@@ -17,7 +17,14 @@ l_goal_amount: list[int] = [100, 200, 300, 400]
 Window.size = (300, 500)
 
 
-class Goal(TwoLineIconListItem):
+class Goal:
+    def __init__(self):
+        self.icon = ""
+        self.intensity = 0
+        self.goal_name = "goal_name"
+
+
+class GoalListItem(TwoLineIconListItem):
     goal_name = StringProperty("goal_name")
     goal_amount = StringProperty("0")
     goal_icon = StringProperty("clock")
@@ -30,6 +37,20 @@ class Goal(TwoLineIconListItem):
 
 
 class MainScreen(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.update_goals()
+
+    def update_goals(self):
+        for i in range(len(l_goal_name)):
+            self.ids.goal_list.add_widget(
+                GoalListItem(
+                    goal_name=l_goal_name[i],
+                    goal_amount=l_goal_amount[i],
+                    goal_icon=l_goal_icon[i],
+                )
+            )
+
     pass
 
 
@@ -38,6 +59,27 @@ class GoalStatsScreen(Screen):
 
 
 class GoalCreationScreen(Screen):
+    end_goal_text = StringProperty("Text")
+
+    def __init__(self, **kw):
+        self.new_goal = Goal()
+        super().__init__(**kw)
+
+    def get_goal_type(self, type):
+        print(type)
+        self.new_goal.icon = type
+        if type == "counter":
+            self.end_goal_text = "What is your End Rep Goal"
+        else:
+            self.end_goal_text = "What is your End Time Goal"
+
+    def get_intensity(self, intensity):
+        print(intensity)
+        self.new_goal.intensity = intensity
+
+    def update_end_goal(self, label):
+        self.ids.end_goal.text = label
+
     pass
 
 
@@ -46,19 +88,10 @@ class InfoScreen(Screen):
 
 
 class UiApp(MDApp):
-    def __init__(self, **kwargs):
-        self.title = "One Percent Better"
-        super().__init__(**kwargs)
+    title = "1% Better"
 
-    # def on_start(self):
-    #     for i in range(len(l_goal_name)):
-    #         self.root.main.ids.goal_list.add_widget(
-    #             Goal(
-    #                 goal_name=l_goal_name[i],
-    #                 goal_amount=l_goal_amount[i],
-    #                 goal_icon=l_goal_icon[i],
-    #             )
-    #         )
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def create_goals(self):
         print("creating goals")
@@ -68,8 +101,9 @@ class UiApp(MDApp):
         print("calling info")
 
     def build(self):
+        # This is where you choose what screens you want to load
         sm = ScreenManager()
-        # sm.add_widget(MainScreen(name="main"))
+        sm.add_widget(MainScreen(name="main"))
         sm.add_widget(GoalCreationScreen(name="goal_creation"))
         # sm.add_widget(GoalStatsScreen(name="goal_stats"))
         return sm
